91bc7e3f63f2b3b831e56fd3fed354d140b5efaa
Benjamin Eberlei <kontakt@beberlei.de>
2021-04-07 18:38:39 +0200

    Initial commit Tideways middleware for Laravel Octane.

---
+++ README.md
@@ -0,0 +1,44 @@
+# Tideways Middleware for Laravel Octane
+
+This package connects a Laravel Octance application with Tideways for PHP
+Monitoring, Profiling and Exception Tracking.
+
+It provides a Laravel HTTP middleware that starts Tideways based on the Laravel
+Request object state.
+
+## Installation
+
+You can install the package via composer:
+
+    composer require tideways/laravel-octane-middleware
+
+## Usage
+
+Register the Middleware in your Laravel Middleware stack:
+
+```php
+class Kernel extends HttpKernel
+{
+    protected $middleware = [
+        \Tideways\LaravelOctane\OctaneMiddleware::class,
+        // ...
+    ];
+
+    // ...
+}
+```
+
+Install Tideways PHP extension for PHP 8 and configure the API Key
+via php.ini or additional configuration files tideways.ini:
+
+```
+tideways.api_key=abcdefg
+```
+
+More details on how to install and get the API Key in the Tideways docs:
+
+https://support.tideways.com/documentation/setup/installation/api-key.html
+
+## License
+
+The MIT License (MIT). Please see License File for more information.
---
+++ src/OctaneMiddleware.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tideways\LaravelOctane;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class OctaneMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     j @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!class_exists('Tideways\Profiler') || php_sapi_name() !== 'cli') {
+            // only run when Tideways is installed and the CLI sapi is used (thats how Swoole/RR work)
+            return $next($request);
+        }
+
+        $developerSession = null;
+        if ($request->query->has('_tideways')) {
+            $developerSession = http_build_query((array) $request->query->get('_tideways'));
+        } else if ($request->headers->has('X-TIDEWAYS-PROFILER')) {
+            $developerSession = $request->headers->get('X-TIDEWAYS-PROFILER');
+        } else if ($request->cookies->has('TIDEWAYS_SESSION')) {
+            $developerSession = $request->cookies->get('TIDEWAYS_SESSION')->getValue();
+        }
+
+        $service = ini_get('tideways.service') ?: 'web';
+
+        \Tideways\Profiler::start(['service' => $service, 'developer_session' => $developerSession]);
+        \Tideways\Profiler::setCustomVariable('http.host', $request->getHttpHost());
+        \Tideways\Profiler::setCustomVariable('http.method', $request->getMethod());
+        \Tideways\Profiler::setCustomVariable('http.url', $request->getPathInfo());
+
+        $referenceId = $request->query->get('_tideways_ref', $request->headers->get('X-Tideways-Ref'));
+        if ($request->cookies->has('TIDEWAYS_REF')) {
+            $referenceId = $request->cookies->get('TIDEWAYS_REF')->getValue();
+        }
+
+        if ($referenceId) {
+            \Tideways\Profiler::setCustomVariable('tw.ref', $referenceId);
+        }
+
+        try {
+            return $this->doHandle($request, $next);
+        } catch (Throwable $e) {
+            \Tideways\Profiler::logException($e);
+            throw $e;
+        } finally {
+            \Tideways\Profiler::stop();
+        }
+    }
+
+    /**
+     * Call the next middleware handler.
+     *
+     * We need this intermediate function as a hook for the Tideways PHP extension.
+     *
+     * @return mixed
+     */
+    private function doHandle(Request $request, Closure $next)
+    {
+        return $next($request);
+    }
+}
