2560ab1dd5faf55291b42acd4dd977d794efaf6a
Tim Düsterhus <tim@tideways-gmbh.com>
2025-12-09 15:07:04 +0100

    Fix handling of the `_tideways` query parameter
    Fixes #27

--- tests/OctaneMiddlewareTest.php
+++ tests/OctaneMiddlewareTest.php
@@ -3,8 +3,10 @@
 namespace Tideways\LaravelOctane;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use PHPUnit\Framework\TestCase;
 use InvalidArgumentException;
+use Symfony\Component\Process\Exception\ProcessTimedOutException;
 use Symfony\Component\Process\PhpExecutableFinder;
 use Symfony\Component\Process\Process;
 
@@ -32,6 +34,8 @@ class OctaneMiddlewareTest extends TestCase
             $middleware->handle($request, function () {
                 throw new InvalidArgumentException('failing');
             });
+        }, function (\Throwable $e) {
+            // ignore
         });
 
        $this->assertCount(2, $payload);
@@ -40,13 +44,64 @@ class OctaneMiddlewareTest extends TestCase
 
        $this->assertEquals(InvalidArgumentException::class, $payload[1]['a']['error.type']);
     }
+
+    public function testTidewaysQueryIsValid(): void
+    {
+        $payload = withTidewaysDaemon(function () {
+            $request = Request::create('/?_tideways[method]=&_tideways[user]=foo&_tideways[time]=2145913200&_tideways[hash]=8627e21a5dd6bc1f97106b61c2b8d914147f207cc4c00d65592ec6463fd93583', 'GET');
+
+            $middleware = new OctaneMiddleware();
+            $response = $middleware->handle($request, function () {
+                return new Response(\Tideways\Profiler::isProfiling() ? 'valid' : 'invalid');
+            });
+
+            $this->assertEquals('valid', $response->getContent());
+        });
+
+        $this->assertEquals('foo', $payload[0]['a']['tw.uid']);
+        $this->assertEquals('cli', $payload[0]['a']['php.sapi']);
+    }
+
+    public function testTidewaysQueryIsInvalid(): void
+    {
+        $payload = withTidewaysDaemon(function () {
+            $request = Request::create('/?_tideways[method]=&_tideways[user]=foo&_tideways[time]=2145913200&_tideways[hash]=invalid', 'GET');
+
+            $middleware = new OctaneMiddleware();
+            $response = $middleware->handle($request, function () {
+                return new Response(\Tideways\Profiler::isProfiling() ? 'valid' : 'invalid');
+            });
+
+            $this->assertEquals('invalid', $response->getContent());
+        });
+
+        $this->assertNull($payload[0]['a']['tw.uid']);
+        $this->assertEquals('cli', $payload[0]['a']['php.sapi']);
+    }
+
+    public function testTidewaysQueryIsIncorrectType(): void
+    {
+        $payload = withTidewaysDaemon(function () {
+            $request = Request::create('/?_tideways=string', 'GET');
+
+            $middleware = new OctaneMiddleware();
+            $response = $middleware->handle($request, function () {
+                return new Response(\Tideways\Profiler::isProfiling() ? 'valid' : 'invalid');
+            });
+
+            $this->assertEquals('invalid', $response->getContent());
+        });
+
+        $this->assertNull($payload[0]['a']['tw.uid']);
+        $this->assertEquals('cli', $payload[0]['a']['php.sapi']);
+    }
 }
 
-function withTidewaysDaemon(\Closure $callback)
+function withTidewaysDaemon(\Closure $callback, ?\Closure $onError = null)
 {
     $address = "tcp://127.0.0.1:64111";
     ini_set('tideways.connection', $address);
-    ini_set('tideways.api_key', 'abcdefg');
+    ini_set('tideways.api_key', 'foo');
     ini_set('tideways.sample_rate', 0);
     ini_set('tideways.monitor', 'basic');
     putenv('TIDEWAYS_CONNECTION=' . $address);
@@ -78,11 +133,21 @@ function withTidewaysDaemon(\Closure $callback)
 
     try {
         $callback();
-    } catch (\Throwable) {
+    } catch (\Throwable $e) {
+        if ($onError !== null) {
+            $onError($e);
+        } else {
+            throw $e;
+        }
+    } finally {
+        try {
+            $daemon->wait();
+        } catch (ProcessTimedOutException $e) {
+            $daemon->stop(1.0, SIGKILL);
 
+            throw $e;
+        }
     }
-
-    $daemon->wait();
 
     return json_decode($daemon->getOutput(), true, flags: JSON_THROW_ON_ERROR);
 }
