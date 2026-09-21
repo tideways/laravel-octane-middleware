2aff9e168b48d15238d258e6c2974a5e5bba14c8
Tim Düsterhus <tim@tideways-gmbh.com>
2026-09-21 15:33:27 +0200

    Fix `Profiler::isProfiling()` assertion in `testTidewaysSessionCookieIsValid()`

--- tests/OctaneMiddlewareTest.php
+++ tests/OctaneMiddlewareTest.php
@@ -32,7 +32,7 @@ final class OctaneMiddlewareTest extends TestCase
             $request->cookies->set('TIDEWAYS_REF', 'bar');
 
             $middleware = new OctaneMiddleware();
-            $middleware->handle($request, function () {
+            $response = $middleware->handle($request, function () {
                 return new Response(\Tideways\Profiler::isProfiling() ? 'valid' : 'invalid');
             });
 
